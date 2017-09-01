24b2f223586f21d887b6c413570f36f5de17504c
Chocolawt <30875154+Chocolawt@users.noreply.github.com>
2017-09-01 09:21:49 -1000

    Committed forlooppractice.js

---
+++ forlooppractice.js
@@ -0,0 +1,4 @@
+var x = ["Hi", "Justin", "Hur"];
+for (var i=0; i< x.length; i++) { //can use x.length instead of 3 because you do not have to keep changing the numbers after you add anorther array
+window.alert(x[i]);
+}
